0dcc444f845f424f1d62b6fe3042d16815ca42fa
talha08 <talhaqc@gmail.com>
2018-12-12 01:58:49 +0600

    express

---
+++ db/connect.js
@@ -0,0 +1,23 @@
+import Mongoose from "mongoose"
+import logger from "../core/logger/app-logger"
+import config from "../core/config/config.dev"
+
+Mongoose.Promise = global.Promise
+
+const connectToDb = async () => {
+  let dbHost = config.dbHost
+  let dbPort = config.dbPort
+  let dbName = config.dbName
+  try {
+    //await Mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, { useMongoClient: true });
+    await Mongoose.connect(
+      `mongodb://test:talha012@ds131784.mlab.com:31784/mongoapp`,
+      { useMongoClient: true, autoIndex: false }
+    )
+    logger.info("Connected to mongo!!!")
+  } catch (err) {
+    logger.error("Could not connect to MongoDB")
+  }
+}
+
+export default connectToDb
